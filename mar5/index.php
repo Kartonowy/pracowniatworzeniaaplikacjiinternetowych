bd7e9e1cc2360e53f6b673f6a268a1f45ad2692e
Kartonowy <ttrollface.com.pl@gmail.com>
2024-06-20 20:56:50 +0200

    fix

--- mar5/index.php
+++ mar5/index.php
@@ -14,13 +14,13 @@
     <body>
         <header>
             <h1>Colories</h1>
-            <div id="beauty-line">
+            <div class="beauty-line">
         </header>
         <aside>
             <h2>Color selection</h2>
+            <p>Write the name of the color down below,  <br>
+                Select a language and voilà.</p>
             <form action="" method="post">
-            <p>Write the name of the color down below,</p>
-            <p>select a language and voilà</p>
                 <input type="text" name="colname" id="">
                 <select name="lang" id="">
                     <option value="polish">pl</option>
@@ -37,12 +37,13 @@
     $query = "SELECT polish, english, french, hex from colories where " . $_POST['lang'] . " = '" . $_POST['colname'] . "'";
     $result = $db->query($query)->fetch_assoc();
     if ($result == null) {
-        echo "<div class='wrapper'> Color was not found, but you can add it! <br>
-        <form method='post'>
-    <input type='text' name='polish' id=''>
-    <input type='text' name='english' id=''>
-    <input type='text' name='french' id=''>
-    <input type='text' name='hex' id=''>
+        echo "<div class='wrapper'> 
+        <form method='post' class='newly'>
+<label>Color was not found, but you can add it!</label>
+    <input type='text' name='polish' id='' placeholder='polish'>
+    <input type='text' name='english' id='' placeholder='english'>
+    <input type='text' name='french' id='' placeholder='french'>
+    <input type='text' name='hex' id='' placeholder='hex code'>
                 <input type='submit' value='Submit'>
 </form> </div>";
 } else {
@@ -64,39 +65,10 @@ echo "
 
 </div>";
     echo sprintf("<script>
-console.log('xd')
-function hexToHSL(hex) {
-  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-    r = parseInt(result[1], 16);
-    g = parseInt(result[2], 16);
-    b = parseInt(result[3], 16);
-    r /= 255, g /= 255, b /= 255;
-    var max = Math.max(r, g, b), min = Math.min(r, g, b);
-    var h, s, l = (max + min) / 2;
-    if(max == min){
-      h = s = 0; // achromatic
-    }else{
-      var d = max - min;
-      s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
-      switch(max){
-        case r: h = (g - b) / d + (g < b ? 6 : 0); break;
-        case g: h = (b - r) / d + 2; break;
-        case b: h = (r - g) / d + 4; break;
-      }
-      h /= 6;
-    }
-  var HSL = new Object();
-  HSL['h']=h;
-  HSL['s']=s;
-  HSL['l']=l;
-  return HSL;
-}
-        let output = hexToHSL(%s)
-        console.log(output)
-        document.documentElement.style.setProperty('--main-color', '%s')
-        document.documentElement.style.setProperty('--bg-color', 'hsl(output['h'], output['s']\%, output['l']\%)')
+        document.querySelector(':root').style.setProperty('--main-color', '%s')
+        document.querySelector(':root').style.setProperty('--main-bg', '%s')
 
-</script>", $result['hex'], $result['hex']);
+</script>", $result['hex'], $result['hex'] . "44");
 }
 }
     if (isset($_POST['polish']) && preg_match($pl_regex, $_POST['polish']) && isset($_POST['english']) && preg_match($en_regex, $_POST['english']) && isset($_POST['french']) && preg_match($fr_regex, $_POST['french']) &&isset($_POST['hex'])) {
@@ -112,6 +84,9 @@ function hexToHSL(hex) {
 }
 ?>
         </aside>
+<footer>
+<div class="beauty-line"></div>
+</footer>
     </body>
 </html>
 <?php 
